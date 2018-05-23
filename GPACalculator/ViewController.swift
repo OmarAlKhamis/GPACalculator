55f618cef98353b2e87b78cf396bc1188ed5e8c2
Omar AlKhamis <omaralkhamis@omars-imac.local>
2018-05-23 23:25:34 +0300

    Initial Commit

---
+++ GPACalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPACalculator
+//
+//  Created by Omar AlKhamis on 08/09/1439 AH.
+//  Copyright © 1439 Omar AlKhamis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
