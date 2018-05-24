8f2f89e477f5efe33815f68b2f639c514aa773d9
Omar AlKhamis <omaralkhamis@omars-imac.lan>
2018-05-24 19:27:26 +0300

    Started working on the calculator brain class

--- GPACalculator/CalculatorBrain.swift
+++ GPACalculator/CalculatorBrain.swift
@@ -10,4 +10,36 @@ import Foundation
 
 class CalculatorBrain{
     
+    var gpaScale:GPAScale?
+    var earnedCreditHours:Int?
+    var earnedQualityPoints:Double?
+    var semesterPerformance = [String:Int]()
+    
+    func CalculatorBrain(){
+        
+    }
+    
+    struct LetterGrades {
+        static let outOf4 = ["A+":4.00,
+                             "A":3.75,
+                             "B+":3.50,
+                             "B":3.00,
+                             "C+":2.50,
+                             "C":2.00,
+                             "D+":1.50,
+                             "D":1.00]
+        
+        static let outOf5 = ["A+":5.00,
+                             "A":4.75,
+                             "B+":4.50,
+                             "B":4.00,
+                             "C+":3.50,
+                             "C":3.00,
+                             "D+":2.50,
+                             "D":2.00]
+    }
+    enum GPAScale {
+        case outOf4
+        case outOf5
+    }
 }
