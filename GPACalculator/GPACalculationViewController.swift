7ed52410a61252261d936dfe02b21dc5d929255c
Omar AlKhamis <omaralkhamis@omars-imac.lan>
2018-05-24 17:33:35 +0300

    writing the classes

---
+++ GPACalculator/GPACalculationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GPACalculationView.swift
+//  GPACalculator
+//
+//  Created by Omar AlKhamis on 09/09/1439 AH.
+//  Copyright © 1439 Omar AlKhamis. All rights reserved.
+//
+
+import UIKit
+
+class GPACalculationViewController:UIViewController {
+    
+    
+    
+}
---
+++ GPACalculator/ImproveGPAViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImproveGPAViewController.swift
+//  GPACalculator
+//
+//  Created by Omar AlKhamis on 09/09/1439 AH.
+//  Copyright © 1439 Omar AlKhamis. All rights reserved.
+//
+
+import UIKit
+
+class ImproveGPAViewController: UIViewController{
+    
+    
+}
