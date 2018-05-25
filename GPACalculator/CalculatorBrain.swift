15a47cd3456298899d614488810d33dd50d71af1
Omar AlKhamis <omaralkhamis@omars-imac.lan>
2018-05-25 17:10:08 +0300

    Writing more in the Calculator brain

--- GPACalculator/CalculatorBrain.swift
+++ GPACalculator/CalculatorBrain.swift
@@ -16,28 +16,15 @@ class CalculatorBrain{
 }
 
 class Semester{
-    private var courses:[Course]
-    private var gpaScale:GPAScale
-    private var attemptedCreditHours:Int{
-        get{
-            var ch = 0
-            for course in self.courses{
-                ch = ch + course.creditHours
-            }
-            return ch
-        }
-    }
-    private var attemptedQualityPoints:Double{
-        get{
-            switch self.gpaScale {
-            case .outOf4:
-                return Double(self.attemptedCreditHours)*LetterGrades.outOf4["A+"]!
-            case .outOf5:
-                return Double(self.attemptedCreditHours)*LetterGrades.outOf5["A+"]!
-            }
-        }
-    }
-    private var earnedQualityPoints:Double{
+    private var courses:[Course]                                                                    // An array holding courses which are represented by course letter grade and course credit hours
+    private var gpaScale:GPAScale                                                                   // Enum selecting the gpaSclae, out of 4 or out of 5
+    private var previousCreditHours:Int                                                             // previously earned credit hours, prior to this semester
+    private var previousGPA:Double                                                                  // previous achieved GPA, prior to this semester
+    private var previousQualityPoints:Double{get{return previousGPA*Double(previousCreditHours)}}   // Previuosely earned quality poinst, prior to this semester
+    private var totalQualityPoints:Double{get{return previousQualityPoints+attemptedQualityPoints}} // Total number of quality points for all semesters
+    private var totalCreditHours:Int{get{return previousCreditHours+attemptedCreditHours}}          // Total number of credit hours, for all sesemter
+    private var attemptedCreditHours:Int{ get{var CH = 0; for course in self.courses{ CH = CH + course.creditHours }; return CH } }  // The total number of attapted credit hourst in this semester
+    private var attemptedQualityPoints:Double{      // Quality points of this semester
         get{
             var qp = 0.0
             for course in self.courses{
@@ -51,21 +38,21 @@ class Semester{
             return qp
         }
     }
-    var semesterGPA:Double{
-        get{
-            return earnedQualityPoints/Double(attemptedCreditHours)
-        }
-    }
-    init(courses:[Course],gpaScale:GPAScale){
+    var semesterGPA:Double{get{return attemptedQualityPoints/Double(attemptedCreditHours)}}         // This semester's GPA
+    var cumulativeGPA:Double{get{return totalQualityPoints/Double(totalCreditHours)}}               // Cumulative GPA for all semester
+    
+    // Constructor of the class, prevoius values will default to 0 if not entered
+    init(courses:[Course],gpaScale:GPAScale,previousCreditHours:Int=0, previousGPA:Double=0.0){
         self.courses = courses
         self.gpaScale = gpaScale
+        self.previousGPA = previousGPA
+        self.previousCreditHours = previousCreditHours
     }
 }
 
 class Course{
     var creditHours:Int
     var letterGrade:String
-
     init (creditHours:Int,letterGrade:String){
         self.creditHours = creditHours
         self.letterGrade = letterGrade
@@ -91,7 +78,7 @@ struct LetterGrades {
                          "C":3.00,
                          "D+":2.50,
                          "D":2.00,
-                         "F":0.00]
+                         "F":1.00,]
 }
 
 enum GPAScale {
